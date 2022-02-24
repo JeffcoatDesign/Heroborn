3a1b88e03d3a3948dbd250ea9a7678400c0d79a0
JeffcoatDesign <98057241+JeffcoatDesign@users.noreply.github.com>
2022-02-24 14:44:41 -0700

    M02 DV02

--- Heroborn/Assets/Scripts/GameBehaviour.cs
+++ Heroborn/Assets/Scripts/GameBehaviour.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using CustomExtensions;
 
-public class GameBehaviour : MonoBehaviour
+public class GameBehaviour : MonoBehaviour, IManager
 {
+    private string _state;
+    public string State
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
     public string labelText = "Collect all 4 items and win your freedom!";
     public int maxItems = 4;
     public bool showWinScreen = false;
@@ -52,11 +59,18 @@ public class GameBehaviour : MonoBehaviour
             }
         }
     }
-    void RestartLevel()
+    private void Start()
     {
-        SceneManager.LoadScene(0);
-        Time.timeScale = 1.0f;
+        Initialize();
     }
+
+    public void Initialize()
+    {
+        _state = "Manager initialized.";
+        _state.FancyDebug();
+        Debug.Log(_state);
+    }
+
     private void OnGUI()
     {
         GUI.Box(new Rect(20, 20, 150, 25), "Player Health: " + _playerHP);
@@ -67,14 +81,14 @@ public class GameBehaviour : MonoBehaviour
         {
             if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "YOU WON!"))
             {
-                RestartLevel();
+                Utilities.RestartLevel(0);
             }
         }
         if (showLossScreen)
         {
             if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 -50, 200, 100), "You lose..."))
             {
-                RestartLevel();
+                Utilities.RestartLevel();
             }
         }
     }
